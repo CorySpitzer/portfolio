23848a2e0fb23f5ae5062e3582f46526d54ba760
Cory Spitzer <coryspitzer@gmail.com>
2015-07-02 14:40:43 -0700

    Add notes about lack of controller testing

--- spec/features/display_add_project_form_page_spec.rb
+++ spec/features/display_add_project_form_page_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
+# Some of this testing should really be done on the class
+# to determine whether the controller or front-end is breaking,
+# but for now this will work:
 describe 'the add project form' do
   it 'adds a project using the form' do
     visit new_project_path
