2e0fcaade2b7c742490ae4b247449e85b85b079b
Cory Spitzer <coryspitzer@gmail.com>
2015-07-02 16:12:37 -0700

    Add error messages to the Project form partial

--- spec/features/display_add_project_form_page_spec.rb
+++ spec/features/display_add_project_form_page_spec.rb
@@ -11,4 +11,25 @@ describe 'the add project form' do
     click_on 'Create Project'
     expect(page).to have_content 'test project1'
   end
+
+  it 'gives an error when nothing is entered' do
+    visit new_project_path
+    click_on 'Create Project'
+    expect(page).to have_content 'errors'
+  end
+
+  # To be thorough, we can also test for the lack of a single field:
+    # it 'gives an error when no description is entered' do
+    #   visit new_project_path
+    #   fill_in 'project_name', with: 'test'
+    #   click_on 'Create Project'
+    #   expect(page).to have_content 'errors'
+    # end
+    #
+    # it 'gives an error when no name is entered' do
+    #   visit new_project_path
+    #   fill_in 'project_description', with: 'test'
+    #   click_on 'Create Project'
+    #   expect(page).to have_content 'errors'
+    # end
 end
