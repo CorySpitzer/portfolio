23c464077a3e270749acd675540a0cbdc05eafe3
Cory Spitzer <coryspitzer@gmail.com>
2015-07-17 17:02:04 -0700

    Try to add ajax scrolling

---
+++ app/assets/javascripts/references.js
@@ -0,0 +1,19 @@
+// $(function() {
+//   $('.pagination').hide()
+//   $(window).scroll(function() {
+//     var url = $('.pagination .next_page').attr('href');
+//     if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
+//       $(window).off("scroll");
+//       $("#references").append("<p id='loading'>Loading more references...</p>");
+//       $.getScript(url);
+//     }
+//   });
+//
+// });
+
+// $(function () {
+//   $('#new-reference, .pagination a').click(function () {
+//     $.getScript(this.href);
+//     return false;
+//   });
+// })
--- app/controllers/references_controller.rb
+++ app/controllers/references_controller.rb
@@ -2,9 +2,15 @@ class ReferencesController < ApplicationController
   respond_to :html, :js
 
   def index
+    # Delay before loading
+    sleep 1
     # Get the references ready for pagination
     @references = Reference.all.paginate(page: params[:page], per_page: 3)
     # @reference = Reference.new
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def new
