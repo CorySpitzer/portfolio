4525bcf1e05e833d3b897fc73636ab26d58438e2
CorySpitzer <coryspitzer@gmail.com>
2015-09-21 12:20:40 -0700

    Remove add project spec
    The add project link is purposely no longer displayed, so testing
    it doesn't make sense.

--- spec/features/reference_form_path_spec.rb
+++ spec/features/reference_form_path_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-describe 'the add reference form' do
-  # because we are testing functionality that requires js,
-  # we need 'js: true':
-  it 'is added to the page after its link is clicked', js: true do
-    visit references_path
-    expect(page).to have_no_content 'info'
-    click_on 'Add Reference'
-    expect(page).to have_content 'info'
-  end
+# describe 'the add reference form' do
+#   # because we are testing functionality that requires js,
+#   # we need 'js: true':
+#   it 'is added to the page after its link is clicked', js: true do
+#     visit references_path
+#     expect(page).to have_no_content 'info'
+#     click_on 'Add Reference'
+#     expect(page).to have_content 'info'
+#   end
 
   # it 'adds a reference', js: true do
   #   visit references_path
@@ -19,4 +19,4 @@ describe 'the add reference form' do
   #   click_on 'Create Reference'
   #   expect(page).to have_content 'Blarb Software'
   # end
-end
+# end
